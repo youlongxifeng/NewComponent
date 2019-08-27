e04b7e7ef983820d6e23bbcd5683e9134c3bf864
youlongxifeng <xie774505257@163.com>
2019-08-27 13:39:56 +0800

    提交代码

---
+++ app/src/main/java/com/component/preject/home/MainActivity.java
@@ -0,0 +1,14 @@
+package com.component.preject.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
